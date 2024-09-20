76962dd80437276b78de9456ac07b14980b4f9a6
godriceeichie <emmaeichie@gmail.com>
2024-09-20 19:54:36 +0100

    changed the api route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://4db2-105-116-7-64.ngrok-free.app/api",
+    baseURL: "https://expense-voyage-backend.onrender.com/api",
     withCredentials: true,
 })
 
 export const privateApi = axios.create({
-    baseURL: "https://4db2-105-116-7-64.ngrok-free.app/api",
+    baseURL: "https://expense-voyage-backend.onrender.com/api",
     withCredentials: true,
 })
